575a101e92d65344fe77a7e2a03d831eda49d14a
Serhii Demchenko <svdemchenko@gmail.com>
2023-01-19 21:11:12 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 
 class App extends Component {
-  state = {};
+  state = {
+    contacts: [],
+    name: '',
+  };
   render() {
-    return;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <from>
+          <div>
+            <label htmlFor="">
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+            </label>
+            <label htmlFor="">
+              <input
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+              />
+            </label>
+            <button>Add contact</button>
+          </div>
+        </from>
+        <h2>Contacts</h2>
+        <div>
+          <label htmlFor="">
+            <input type="text" />
+          </label>
+          <ul></ul>
+        </div>
+      </div>
+    );
   }
 }
 
